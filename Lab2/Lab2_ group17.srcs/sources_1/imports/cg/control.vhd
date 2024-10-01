02484617247af81ad0121267853129420a5e8192
Kristian Goystdal <krisgoy100@gmail.com>
2024-10-01 15:44:19 +0100

    Refactor control.vhd to improve state transitions and signal assignments

--- Lab2/Lab2_ group17.srcs/sources_1/imports/cg/control.vhd
+++ Lab2/Lab2_ group17.srcs/sources_1/imports/cg/control.vhd
@@ -11,16 +11,19 @@ ENTITY control IS
     clk, rst : IN STD_LOGIC;
     instr : IN STD_LOGIC;
     btnU, btnD, btnL, btnR : IN STD_LOGIC;
-    mux_enables : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
-    reg_enables : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
-    alu_selectors : OUT STD_LOGIC_VECTOR (2 DOWNTO 0)
+    selectors : OUT STD_LOGIC_VECTOR (9 DOWNTO 0);
+    enables : OUT STD_LOGIC_VECTOR (6 DOWNTO 0);
+    done : OUT STD_LOGIC;
   );
 END control;
 
 ARCHITECTURE Behavioral OF control IS
-  TYPE fsm_states IS (s_initial, s_end, s_subs, s_adds, s_muls, s_or, s_sra, s_load1, s_load2);
+  TYPE fsm_states IS (s_initial, s_end, s_cycle1, s_cycle2, s_cycle3, s_cycle4, s_cycle15, s_load);
   SIGNAL currstate, nextstate : fsm_states;
 
+  SIGNAL input_addr : STD_LOGIC_VECTOR(9 DOWNTO 0);
+  SIGNAL input_addr_next : STD_LOGIC_VECTOR(9 DOWNTO 0);
+  SIGNAL output_addr : STD_LOGIC_VECTOR(9 DOWNTO 0);
 BEGIN
   state_reg : PROCESS (clk)
   BEGIN
@@ -29,6 +32,7 @@ BEGIN
         currstate <= s_initial;
       ELSE
         currstate <= nextstate;
+        input_addr <= input_addr_next;
       END IF;
     END IF;
   END PROCESS;
@@ -37,77 +41,55 @@ BEGIN
   BEGIN --  process
 
     nextstate <= currstate; -- by default, does not change the state.
+    input_addr_next <= input_addr; -- by default, does not change the input_addr.
 
     CASE currstate IS
       WHEN s_initial =>
-        IF btnU = '1' AND instr = '0' THEN
-          nextstate <= s_adds;
-        ELSIF btnU = '1' AND instr = '1' THEN
-          nextstate <= s_subs;
-        ELSIF btnR = '1' THEN
-          nextstate <= s_muls;
-        ELSIF btnD = '1' AND instr = '0' THEN
-          nextstate <= s_or;
-        ELSIF btnD = '1' AND instr = '1' THEN
-          nextstate <= s_sra;
-        ELSIF btnL = '1' AND instr = '0' THEN
-          nextstate <= s_load1;
-        ELSIF btnL = '1' AND instr = '1' THEN
-          nextstate <= s_load2;
+        IF rst = '0' THEN
+          nextstate <= s_load;
+          done <= '0';
         END IF;
-        mux_enables <= "XX";
-        reg_enables <= "00";
-        alu_selectors <= "000";
+        selectors <= "XXXXXXXXXX";
+        enables <= "0000000";
 
-      WHEN s_adds =>
-        nextstate <= s_end;
-        mux_enables <= "11";
-        reg_enables <= "10";
-        alu_selectors <= "000";
+      WHEN s_load =>
+        nextstate <= s_cycle1;
+        selectors <= "0000000000";
+        enables <= "X111111";
 
-      WHEN s_subs =>
-        nextstate <= s_end;
-        mux_enables <= "11";
-        reg_enables <= "10";
-        alu_selectors <= "100";
+      WHEN s_cycle1 =>
+        nextstate <= s_cycle2;
+        selectors <= "0011100110";
+        enables <= "0011100";
 
-      WHEN s_muls =>
-        nextstate <= s_end;
-        mux_enables <= "11";
-        reg_enables <= "10";
-        alu_selectors <= "001";
+      WHEN s_cycle2 =>
+        nextstate <= s_cycle3;
+        selectors <= "0XXXXX1101";
+        enables <= "0000011";
 
-      WHEN s_or =>
-        nextstate <= s_end;
-        mux_enables <= "11";
-        reg_enables <= "10";
-        alu_selectors <= "010";
+      WHEN s_cycle3 =>
+        nextstate <= s_cycle4;
+        selectors <= "0000000000";
+        enables <= "0000000";
 
-      WHEN s_sra =>
-        nextstate <= s_end;
-        mux_enables <= "11";
-        reg_enables <= "10";
-        alu_selectors <= "011";
+      WHEN s_cycle4 =>
+        nextstate <= s_cycle5;
+        selectors <= "0000000000";
+        enables <= "0000000";
 
-      WHEN s_load1 =>
+      WHEN s_cycle5 =>
         nextstate <= s_end;
-        mux_enables <= "00";
-        reg_enables <= "01";
-        alu_selectors <= "000";
-
-      WHEN s_load2 =>
-        nextstate <= s_end;
-        mux_enables <= "10";
-        reg_enables <= "10";
-        alu_selectors <= "000";
+        selectors <= "0000000000";
+        enables <= "0000000";
 
       WHEN s_end =>
-        IF btnU = '0' AND btnR = '0' AND btnD = '0' AND btnL = '0' THEN
+        IF instr = '0' THEN
           nextstate <= s_initial;
+          input_addr_next <= std_logic_vector(unsigned(input_addr) + 1); -- Increment input_addr
+
         END IF;
-        mux_enables <= "XX";
-        reg_enables <= "00";
-        alu_selectors <= "000";
+        selectors <= "XXXXXXXXXX";
+        enables <= "0000000";
 
     END CASE;
   END PROCESS;
