ef6c3e67b32b7def8cc45d96ea7b411b5a237bf2
Einar Bergslid <einarbergslid@gmail.com>
2024-10-01 15:42:55 +0100

    datapath finished - before any testing

--- Lab2/Lab2_ group17.srcs/sources_1/imports/cg/OLD_datapath.vhd
+++ Lab2/Lab2_ group17.srcs/sources_1/imports/cg/OLD_datapath.vhd
@@ -3,8 +3,7 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY datapath IS
-  PORT
-  (
+  PORT (
     value_sm : IN STD_LOGIC_VECTOR (9 DOWNTO 0); -- input-data in format sign-magnitude
     rst, en_r1, en_r2, sel_mux1, sel_mux2, sel_add_sub, clk : IN STD_LOGIC; -- single bit control signals
     sel_mux_alu : IN STD_LOGIC_VECTOR (1 DOWNTO 0); -- 2bit control signals
@@ -24,8 +23,8 @@ BEGIN
   value_mag <= unsigned(value_sm(8 DOWNTO 0));
 
   -- Convert from sign-magnitude to signed
-  value <= - signed(resize(value_mag, 16)) WHEN value_sm(9) = '1' ELSE 
-             signed(resize(value_mag, 16));
+  value <= -signed(resize(value_mag, 16)) WHEN value_sm(9) = '1' ELSE
+           signed(resize(value_mag, 16));
 
   -- adder/subtracter
   res_addsub <= r1_out + r2_out WHEN sel_add_sub = '0' ELSE
@@ -43,20 +42,20 @@ BEGIN
   res_sra <= signed(res_sra_slv);
 
   -- ALU
-  WITH sel_mux_alu SELECT 
+  WITH sel_mux_alu SELECT
     mux_alu <= res_addsub WHEN "00",
-    res_mul(15 DOWNTO 0) WHEN  "01",
-    res_or WHEN  "10",
+    res_mul(15 DOWNTO 0) WHEN "01",
+    res_or WHEN "10",
     res_sra WHEN OTHERS;
 
   -- mux 1
   WITH sel_mux1 SELECT
-    mux1_out <= value WHEN  '0',
+    mux1_out <= value WHEN '0',
     mux_alu WHEN OTHERS;
 
   -- mux 2
   WITH sel_mux2 SELECT
-    mux2_out <= r1_out WHEN  '0',
+    mux2_out <= r1_out WHEN '0',
     r2_out WHEN OTHERS;
 
   -- register R1
--- Lab2/Lab2_ group17.srcs/sources_1/imports/cg/datapath.vhd
+++ Lab2/Lab2_ group17.srcs/sources_1/imports/cg/datapath.vhd
@@ -4,26 +4,37 @@ USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY datapath IS
   PORT (
-  A, B, C, D, E, F : IN signed (15 DOWNTO 0); -- Input data
+    A, B, C, D, E, F : IN signed (15 DOWNTO 0); -- Input data
 
-  S1, S2, S3, S4, S6, S7, S8, S9 : IN STD_LOGIC; -- Mux seletcs
-  S5 : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+    S1, S2, S3, S4, S6, S7, S8, S9 : IN STD_LOGIC; -- Mux seletcs
+    S5 : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
 
-  E1, E2, E3, E4, E5, E6, E7 : IN STD_LOGIC; -- Enables (6 registers and 1 SRA)
+    E1, E2, E3, E4, E5, E6, E7 : IN STD_LOGIC; -- Enables (6 registers and 1 SRA)
 
-  CLK, RST : IN STD_LOGIC; -- Clock and synchronous active high reset
+    CLK, RST : IN STD_LOGIC; -- Clock and synchronous active high reset
 
-  DATA_OUT : OUT signed (31 DOWNTO 0); -- Output data
+    DATA_OUT : OUT signed (31 DOWNTO 0)); -- Output data
 END datapath;
 
 ARCHITECTURE behavioral OF datapath IS
-  SIGNAL mul1_out, mul2_out, alu_out, sra_out, mux2, mux3, mux3, mux4, mux5, mux6, mux7, mux8 : signed (31 DOWNTO 0);
-  SIGNAL mux1 : signed (15 DOWNTO 0);
-  SIGNAL r1, r2, r3, r4, r5, r6 : signed (31 DOWNTO 0);
+
+  SIGNAL mul1_out, mul2_out, alu_out, sra_out : signed (31 DOWNTO 0); -- Operator outputs
+
+  SIGNAL mux1, mux2, mux3, mux3, mux4, mux5, mux6, mux7, mux8 : signed (31 DOWNTO 0); -- Mux outputs
+
+  SIGNAL r1, r2, r3, r4, r5, r6 : signed (31 DOWNTO 0); --Register "outputs" 
 
 BEGIN
 
   -- Muxes:
+
+  WITH S1 SELECT
+    mux1 <= r1 WHEN '0',
+    r3 WHEN OTHERS;
+
+  WITH S2 SELECT
+    mux2 <= r1 WHEN '0',
+    r3 WHEN OTHERS;
 
   WITH S3 SELECT
     mux3 <= resize(A, 32) WHEN '0',
@@ -40,7 +51,7 @@ BEGIN
 
   WITH S6 SELECT
     mux6 <= resize(D, 32) WHEN '0',
-    sra_out WHEN OTHERS;
+    mul2_out WHEN OTHERS;
 
   WITH S7 SELECT
     mux7 <= resize(E, 32) WHEN '0',
@@ -117,5 +128,25 @@ BEGIN
       END IF;
     END IF;
   END PROCESS;
+
+  -- Multipliers
+
+  mul1_out <= (mux1 * r2)(31 DOWNTO 0);
+
+  mul2_out <= (r5 * r6)(31 DOWNTO 0);
+
+  --ALU
+
+  WITH S9 SELECT
+    alu_out <= mux2 + r4 WHEN S2 = '0',
+    mux2 - r4 WHEN OTHERS;
+
+  --SRA (integer division)
+
+  WITH E7 SELECT
+    sra_out <= r4 WHEN '0',
+    r4 SRA 2 WHEN OTHERS;
+
+  DATA_OUT <= r6;
 
 END behavioral;
