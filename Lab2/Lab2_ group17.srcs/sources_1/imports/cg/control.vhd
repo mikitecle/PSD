84b7fa247118791992cfb79b4cb43e6ae446e11d
Kristian Goystdal <krisgoy100@gmail.com>
2024-10-01 20:05:39 +0100

    Refactor control.vhd to improve state transitions, signal assignments, and address handling

--- Lab2/Lab2_ group17.srcs/sources_1/imports/cg/control.vhd
+++ Lab2/Lab2_ group17.srcs/sources_1/imports/cg/control.vhd
@@ -1,5 +1,6 @@
 LIBRARY ieee;
 USE ieee.STD_LOGIC_1164.ALL;
+USE ieee.numeric_std.ALL;
 
 ---- Uncomment the following library declaration if instantiating
 ---- any Xilinx primitives in this code.
@@ -10,24 +11,27 @@ ENTITY control IS
   PORT (
     clk, rst : IN STD_LOGIC;
     instr : IN STD_LOGIC;
-    btnU, btnD, btnL, btnR : IN STD_LOGIC;
     selectors : OUT STD_LOGIC_VECTOR (9 DOWNTO 0);
     enables : OUT STD_LOGIC_VECTOR (6 DOWNTO 0);
+    write_det_enable : OUT STD_LOGIC;
     input_addr : OUT STD_LOGIC_VECTOR (9 DOWNTO 0);
     output_addr : OUT STD_LOGIC_VECTOR (9 DOWNTO 0);
-    done : OUT STD_LOGIC;
+    done : OUT STD_LOGIC
   );
 END control;
 
 ARCHITECTURE Behavioral OF control IS
-  TYPE fsm_states IS (s_initial, s_end, s_cycle1, s_cycle2, s_cycle3, s_cycle4, s_cycle15, s_load);
+  TYPE fsm_states IS (s_initial, s_end, s_cycle1, s_cycle2, s_cycle3, s_cycle4, s_cycle5, s_load);
   SIGNAL currstate, nextstate : fsm_states;
-  SIGNAL done_internal : STD_LOGIC;
+  SIGNAL done_internal : STD_LOGIC := '0';
   SIGNAL input_addr_sig : STD_LOGIC_VECTOR (9 DOWNTO 0) := "0000000000";
+  SIGNAL output_addr_sig : STD_LOGIC_VECTOR (9 DOWNTO 0) := "0000000000";
+  SIGNAL done_delayed : STD_LOGIC_VECTOR (4 DOWNTO 0) := "00000";
 
 BEGIN
 
   input_addr <= input_addr_sig;
+  output_addr <= output_addr_sig;
 
   state_reg : PROCESS (clk)
   BEGIN
@@ -36,18 +40,19 @@ BEGIN
         currstate <= s_initial;
       ELSE
         currstate <= nextstate;
-        IF done_internal = '1' THEN
-          input_addr_sig <= STD_LOGIC_VECTOR(unsigned(input_addr_sig) + 1); -- Increment input_addr   
-          done_internal <= '0';
-        END IF;
+        -- input_addr <= input_addr_sig;
+        -- output_addr <= output_addr_sig;
       END IF;
     END IF;
   END PROCESS;
 
-  state_comb : PROCESS (currstate, instr, btnU, btnD, btnL, btnR)
+  state_comb : PROCESS (currstate, rst)
   BEGIN --  process
 
     nextstate <= currstate; -- by default, does not change the state.
+    write_det_enable <= '0'; -- by default, does not change the write_det_enable signal.
+    done_internal <= '0'; -- by default, does not change the done_internal signal.
+    done <= '0'; -- by default, does not change the done signal.
 
     CASE currstate IS
       WHEN s_initial =>
@@ -83,23 +88,47 @@ BEGIN
         enables <= "0000000";
 
       WHEN s_cycle5 =>
-        IF input_addr = "000001111" THEN
+        IF input_addr_sig = "0000001111" THEN
           nextstate <= s_end;
         ELSE
-          nextstate <= s_cycle1;
-          done_internal <= '1';
+          nextstate <= s_load;
         END IF;
         selectors <= "0000000000";
         enables <= "0000000";
+        write_det_enable <= '1';
+        done_internal <= '1';
 
       WHEN s_end =>
-        IF instr = '0' THEN
-          nextstate <= s_initial;
-          done <= '1';
-        END IF;
         selectors <= "XXXXXXXXXX";
         enables <= "0000000";
-
+        done <= '1';
     END CASE;
+  END PROCESS;
+
+  counter_input : PROCESS (clk)
+  BEGIN
+    IF rising_edge(clk) THEN
+      IF rst = '1' THEN
+        input_addr_sig <= "0000000000";
+      ELSE
+        IF done_internal = '1' THEN
+          input_addr_sig <= STD_LOGIC_VECTOR(unsigned(input_addr_sig) + 1); -- Increment input_addr
+        END IF;
+      END IF;
+    END IF;
+  END PROCESS;
+
+  counter_output : PROCESS (clk)
+  BEGIN
+    IF rising_edge(clk) THEN
+      done_delayed <= done_delayed(3 DOWNTO 0) & done_internal;
+      IF rst = '1' THEN
+        output_addr_sig <= "0000000000";
+      ELSE
+        IF done_delayed(4) = '1' THEN
+          output_addr_sig <= STD_LOGIC_VECTOR(unsigned(output_addr_sig) + 1); -- Increment output_addr
+        END IF;
+      END IF;
+    END IF;
   END PROCESS;
 END Behavioral;
